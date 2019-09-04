fb067e93aa3f18e1e8a0f70c631dd38e754bcef7
Luke Hedin <lukehedin@gmail.com>
2019-09-04 15:22:49 +1000

    Added static index file for production build, heroku build command

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
+app.use(bodyParser.json());
+
+//db config here
 
 app.get('/api/customers', (req, res) => {
   const customers = [
@@ -11,6 +16,15 @@ app.get('/api/customers', (req, res) => {
 
   res.json(customers);
 });
+
+if(process.env.NODE_ENV === 'production') {
+	//Serve static assets if in prod
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = 5000;
 
