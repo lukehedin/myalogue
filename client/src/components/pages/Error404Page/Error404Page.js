87b16349ebade8d88229d294e5419401655a34c1
Luke Hedin <lukehedin@gmail.com>
2019-09-19 13:24:56 +1000

    Fixed PWA start url, styled 404 page a little

--- client/src/components/pages/Error404Page/Error404Page.js
+++ client/src/components/pages/Error404Page/Error404Page.js
@@ -11,10 +11,12 @@ export default class Error404Page extends Component {
 		return <div className="page-error-404">
 			<div className="panel-standard">
 				<div className="container">
-						<div className="row">
-							<h2>Page not found</h2>
-							<p>Sorry, the page requested wasn't found. The link may be old or broken.</p>
+					<div className="row">
+						<h2>Page not found</h2>
+						<p className="center">Sorry, the page requested wasn't found. The link may be old or broken.</p>
+						<div className="button-container">
 							<Button to={Util.route.home()} size="lg" colour="pink" label="Back to home" />
+						</div>
 					</div>
 				</div>
 			</div>
